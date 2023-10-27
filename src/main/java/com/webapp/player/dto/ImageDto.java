2df528260ffce551c5c36efa6ce6f99d3f3cf939
AlberdaNazarius <103203230+AlberdaNazarius@users.noreply.github.com>
2023-10-28 01:47:57 +0300

    Added  PlaylistDto to send needed data to frontend

--- src/main/java/com/webapp/player/dto/ImageDto.java
+++ src/main/java/com/webapp/player/dto/ImageDto.java
@@ -1,18 +1,14 @@
 package com.webapp.player.dto;
 
-import com.webapp.player.persistence.entity.Song;
 import lombok.AccessLevel;
 import lombok.Builder;
 import lombok.Value;
 import lombok.experimental.FieldDefaults;
 
-import java.util.Set;
-
 @Value
 @Builder
 @FieldDefaults(level = AccessLevel.PRIVATE)
-public class PlaylistDto {
-  String name;
-  ImageDto image;
-  Set<Song> songs;
+public class ImageDto {
+  String type;
+  byte[] imageData;
 }
